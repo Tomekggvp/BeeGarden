49f5276f80e419fbb599f8d5ce5dd60ffeef64ea
Tomekggvp <tomek2005@mail.ru>
2026-02-14 17:42:13 +0300

    refactor: installing dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import TemporaryDrawer from './componentsMUI/TemporaryDrawer'
+
+
+function App() {
+ 
+
+  return (
+    <>
+        <TemporaryDrawer/>
+        
+    </>
+  )
+}
+
+export default App
